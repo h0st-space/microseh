b9d35af2ac3747f31c7042bf418a0435cf4463c7
Riven Skaye <riven@tae.moe>
2024-10-01 14:12:44 +0200

    build.rs is somehow not picking up cfg directives

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
+#[cfg(not(docsrs))]
 extern crate cc;
 
 #[cfg(not(docsrs))]
 fn main() {
+    if std::env::var("HOST").unwrap().contains("gnu") { return; }
     cc::Build::new().file("src/stub.c").compile("sehstub");
 }
 
