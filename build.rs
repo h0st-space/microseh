38d8c3a43d6ee30eb58d6cdd2affc46680bcddf6
gallexme <lostchaos3@gmail.com>
2024-10-05 14:19:01 +0200

    Update build.rs

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ extern crate cc;
 fn main() {
     // TODO: this is a hack to allow this crate to build on docs.rs.
     //       https://github.com/sonodima/microseh/pull/11#issuecomment-2385633164
-    if !std::env::var("HOST").unwrap_or_default().contains("gnu") {
+    if !std::env::var("CARGO_CFG_TARGET_ENV").unwrap_or_default().contains("gnu") {
         cc::Build::new().file("src/stub.c").compile("sehstub");
     }
 }
